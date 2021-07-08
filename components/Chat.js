2c5bf61ce79db455bedcc5aec1dc4c58275b46be
Yuriko Hijikata <akanesasu3@gmail.com>
2021-07-08 23:29:33 +0900

    added customQueries & modified schema to include sender & receiver info in Chatroom

--- components/Chat.js
+++ components/Chat.js
@@ -1,17 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { View, Text, StyleSheet, TextInput, Button, FlatList } from "react-native";
 
 import { API, graphqlOperation } from "aws-amplify";
 
 import { createMessage, updateChatRoom } from "../src/graphql/mutations";
+import { UserContext } from "../contexts/UserContext";
 
 export default function Chat({ route, navigation }) {
+  const { userDataInfo } = useContext(UserContext);
+  const [userData] = userDataInfo;
   const { user2, chatRoomData } = route.params;
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState("");
 
-  function renderItem(item) {
-    return <Text>{item.item.content}</Text>;
+  function renderItem(message) {
+    return <Message message={message} />;
   }
 
   useEffect(() => {
@@ -24,17 +27,17 @@ export default function Chat({ route, navigation }) {
     <View>
       <Button
         onPress={() => {
-          // router go back to matchlist
           navigation.navigate("MatchList");
         }}
         title="戻る"
         color="#841584"
       />
-      <Text>{user2.name}とお話しましょう！</Text>
+      <Text style={styles.header}>
+        {user2.name}とお話しましょう！　あなた：{userData.name}
+      </Text>
 
       <FlatList data={messages} renderItem={renderItem} />
 
-      <Text>Input: {input}</Text>
       <TextInput
         placeholder="this is text input"
         onChangeText={setInput}
@@ -51,10 +54,11 @@ export default function Chat({ route, navigation }) {
                 input: {
                   chatRoomId: chatRoomData.id,
                   content: input,
+                  sender_id: userData.id,
+                  receiver_id: user2.id,
                 },
               })
             );
-            // TODO: set sender_id to my own & set receiver_id to the other person's
 
             const newMessage = newMessageData.data.createMessage;
             console.log(
@@ -70,10 +74,30 @@ export default function Chat({ route, navigation }) {
   );
 }
 
+const Message = (message) => {
+  const sender_name = message.message.item.sender.name;
+  const content = message.message.item.content;
+  const timestamp = message.message.item.createdAt;
+  console.log({ message });
+  return (
+    <>
+      <Text>
+        {sender_name}: {content} @ {timestamp}
+      </Text>
+    </>
+  );
+};
+
 const styles = StyleSheet.create({
   input: {
     height: 40,
     margin: 12,
     borderWidth: 1,
+  },
+  header: {
+    textAlign: "center",
+    fontSize: 20,
+    backgroundColor: "orange",
+    padding: 10,
   },
 });
