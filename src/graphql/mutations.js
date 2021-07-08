de9d5f55d90640e7cee014827340f589ee39cea7
Yuriko Hijikata <akanesasu3@gmail.com>
2021-07-08 16:00:36 +0900

    initialized amplify & added api, auth

---
+++ src/graphql/mutations.js
@@ -0,0 +1,195 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createCustomer = /* GraphQL */ `
+  mutation CreateCustomer(
+    $input: CreateCustomerInput!
+    $condition: ModelCustomerConditionInput
+  ) {
+    createCustomer(input: $input, condition: $condition) {
+      gender
+      id
+      interests
+      likes {
+        id
+        like
+      }
+      location
+      matches
+      name
+      photo
+      profileText
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateCustomer = /* GraphQL */ `
+  mutation UpdateCustomer(
+    $input: UpdateCustomerInput!
+    $condition: ModelCustomerConditionInput
+  ) {
+    updateCustomer(input: $input, condition: $condition) {
+      gender
+      id
+      interests
+      likes {
+        id
+        like
+      }
+      location
+      matches
+      name
+      photo
+      profileText
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteCustomer = /* GraphQL */ `
+  mutation DeleteCustomer(
+    $input: DeleteCustomerInput!
+    $condition: ModelCustomerConditionInput
+  ) {
+    deleteCustomer(input: $input, condition: $condition) {
+      gender
+      id
+      interests
+      likes {
+        id
+        like
+      }
+      location
+      matches
+      name
+      photo
+      profileText
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const createChatRoom = /* GraphQL */ `
+  mutation CreateChatRoom(
+    $input: CreateChatRoomInput!
+    $condition: ModelChatRoomConditionInput
+  ) {
+    createChatRoom(input: $input, condition: $condition) {
+      id
+      users
+      messages {
+        items {
+          id
+          chatRoomId
+          content
+          createdAt
+          sender
+          receiver
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateChatRoom = /* GraphQL */ `
+  mutation UpdateChatRoom(
+    $input: UpdateChatRoomInput!
+    $condition: ModelChatRoomConditionInput
+  ) {
+    updateChatRoom(input: $input, condition: $condition) {
+      id
+      users
+      messages {
+        items {
+          id
+          chatRoomId
+          content
+          createdAt
+          sender
+          receiver
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteChatRoom = /* GraphQL */ `
+  mutation DeleteChatRoom(
+    $input: DeleteChatRoomInput!
+    $condition: ModelChatRoomConditionInput
+  ) {
+    deleteChatRoom(input: $input, condition: $condition) {
+      id
+      users
+      messages {
+        items {
+          id
+          chatRoomId
+          content
+          createdAt
+          sender
+          receiver
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const createMessage = /* GraphQL */ `
+  mutation CreateMessage(
+    $input: CreateMessageInput!
+    $condition: ModelMessageConditionInput
+  ) {
+    createMessage(input: $input, condition: $condition) {
+      id
+      chatRoomId
+      content
+      createdAt
+      sender
+      receiver
+      updatedAt
+    }
+  }
+`;
+export const updateMessage = /* GraphQL */ `
+  mutation UpdateMessage(
+    $input: UpdateMessageInput!
+    $condition: ModelMessageConditionInput
+  ) {
+    updateMessage(input: $input, condition: $condition) {
+      id
+      chatRoomId
+      content
+      createdAt
+      sender
+      receiver
+      updatedAt
+    }
+  }
+`;
+export const deleteMessage = /* GraphQL */ `
+  mutation DeleteMessage(
+    $input: DeleteMessageInput!
+    $condition: ModelMessageConditionInput
+  ) {
+    deleteMessage(input: $input, condition: $condition) {
+      id
+      chatRoomId
+      content
+      createdAt
+      sender
+      receiver
+      updatedAt
+    }
+  }
+`;
