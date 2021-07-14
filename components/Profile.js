356b85a9ac88ee6b0ca33229845359d16a222f2d
Yuriko Hijikata <akanesasu3@gmail.com>
2021-07-14 22:00:38 +0900

    created global stylesheet & added more styling to MatchPage

--- components/Profile.js
+++ components/Profile.js
@@ -7,7 +7,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import {
   Text,
-  StyleSheet,
   TextInput,
   SafeAreaView,
   TouchableOpacity,
@@ -16,6 +15,8 @@ import {
   View,
   ScrollView,
 } from "react-native";
+
+import { globalStyles } from "../styles/style";
 
 import { AntDesign } from "@expo/vector-icons";
 
@@ -43,34 +44,37 @@ export default function Profile({ navigation }) {
   return (
     <SafeAreaView>
       <ScrollView>
-        <View style={styles.imgContainer}>
-          <Image style={styles.logo} source={require("../assets/profile_logo.png")} />
+        <View style={globalStyles.imgContainer}>
+          <Image
+            style={globalStyles.largeLogo}
+            source={require("../assets/profile_logo.png")}
+          />
         </View>
 
-        <Text style={styles.header}>
+        <Text style={globalStyles.header}>
           {isNewUser ? "初めまして！" : "プロフィールを編集する"}
         </Text>
-        <Text style={styles.inputLabel}>お名前は……</Text>
+        <Text style={globalStyles.inputLabel}>お名前は……</Text>
         <TextInput
-          style={styles.input}
+          style={globalStyles.input}
           onChangeText={setName}
           value={name}
           placeholder="名前（ニックネーム）"
           required
         />
 
-        <Text style={styles.inputLabel}>自己紹介しましょう！</Text>
+        <Text style={globalStyles.inputLabel}>自己紹介しましょう！</Text>
 
         <TextInput
-          style={[styles.input, styles.miltiInput]}
+          style={[globalStyles.input, globalStyles.miltiInput]}
           onChangeText={setProfileText}
           value={profileText}
           placeholder="自己紹介しましょう！"
           multiline={true}
         />
-        <View style={styles.iconContainer}>
+        <View style={globalStyles.iconContainer}>
           <AntDesign name="leftcircle" size={56} color="#F3B614" />
-          <Text style={styles.header}> 1 of 4 </Text>
+          <Text style={globalStyles.header}> 1 of 4 </Text>
 
           <TouchableOpacity
             onPress={() => {
@@ -93,65 +97,3 @@ export default function Profile({ navigation }) {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  imgContainer: {
-    marginVertical: 5,
-
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  iconContainer: {
-    marginHorizontal: 15,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  header: {
-    fontSize: 28,
-    textAlign: "center",
-    color: "#004DA9",
-    fontWeight: "bold",
-    paddingVertical: 20,
-  },
-  logo: {
-    width: 100,
-    height: 100,
-    marginHorizontal: "auto",
-  },
-  input: {
-    height: 40,
-    marginBottom: 12,
-    marginHorizontal: 12,
-    borderWidth: 2,
-    padding: 8,
-    paddingHorizontal: 20,
-    borderColor: "#0093ED",
-    color: "#0093ED",
-    fontSize: 20,
-    borderRadius: 16,
-  },
-  inputLabel: {
-    margin: 12,
-    color: "#0094CE",
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  miltiInput: {
-    height: 200,
-    // backgroundColor: "pink",
-  },
-  label: {
-    fontWeight: "bold",
-    color: "#fff",
-    backgroundColor: "#0094CE",
-    borderRadius: 10,
-    paddingBottom: 5,
-    paddingTop: 5,
-    paddingRight: 10,
-    paddingLeft: 10,
-    marginHorizontal: 3,
-    marginVertical: 5,
-  },
-});
