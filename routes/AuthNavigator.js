5683cab30beff90cc9099574ff1fbe8698f6421f
Yuriko Hijikata <akanesasu3@gmail.com>
2021-07-17 20:21:38 +0900

    added password reset & password confirmation components

--- routes/AuthNavigator.js
+++ routes/AuthNavigator.js
@@ -4,6 +4,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import SignIn from "../components/authentication/SignIn";
 import SignUp from "../components/authentication/SignUp";
+import Confirmation from "../components/authentication/Confirmation";
+import PasswordReset from "../components/authentication/PasswordReset";
+import PasswordConfirmation from "../components/authentication/PasswordConfirmation";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -18,6 +21,9 @@ function AuthNavigator() {
       >
         <Screen name="SignIn" component={SignIn} />
         <Screen name="SignUp" component={SignUp} />
+        <Screen name="Confirmation" component={Confirmation} />
+        <Screen name="PasswordReset" component={PasswordReset} />
+        <Screen name="PasswordConfirmation" component={PasswordConfirmation} />
       </Navigator>
     </NavigationContainer>
   );
