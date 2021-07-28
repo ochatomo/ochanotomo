f289988b06b4aa59b0021b0c3a4955a277804caf
Yuriko Hijikata <akanesasu3@gmail.com>
2021-07-28 18:44:04 +0900

    cleaned code in authentication

--- components/ads/Interstitial.js
+++ components/ads/Interstitial.js
@@ -1,15 +1,13 @@
-import React from "react";
-import { Platform, View, Alert } from "react-native";
+import { Platform } from "react-native";
 import { AdMobInterstitial } from "expo-ads-admob";
 
 async function Interstitial() {
   if (__DEV__) {
-    AdMobInterstitial.setAdUnitID("ca-app-pub-3940256099942544/1033173712"); // テスト広告
+    AdMobInterstitial.setAdUnitID("ca-app-pub-3940256099942544/1033173712");
   } else {
     if (Platform.OS === "ios") {
-      AdMobInterstitial.setAdUnitID("ca-app-pub-4269855195582744/7445874741"); //iOS
-    } else {
-      AdMobInterstitial.setAdUnitID("ca-app-pub-4269855195582744/7445874741"); //android
+      AdMobInterstitial.setAdUnitID("ca-app-pub-4269855195582744/7445874741");
+      AdMobInterstitial.setAdUnitID("ca-app-pub-4269855195582744/7445874741");
     }
   }
   await AdMobInterstitial.requestAdAsync();
--- components/authentication/Confirmation.js
+++ components/authentication/Confirmation.js
@@ -5,7 +5,7 @@ import { globalStyles } from "../../styles/globalStyle";
 import { Colors } from "../../styles/color";
 import { createAlert } from "../../utils/helper";
 
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function Comfirmation({ route, navigation }) {
   const { email } = route.params;
@@ -14,7 +14,6 @@ export default function Comfirmation({ route, navigation }) {
 
   async function confirmSignUp() {
     try {
-      // console.log("email---", email);
       await Auth.confirmSignUp(emailInput, code);
       createAlert(
         "アカウントが作成されました",
@@ -24,7 +23,6 @@ export default function Comfirmation({ route, navigation }) {
         }
       );
     } catch (error) {
-      // console.log("equality check", error.name === "NotAuthorizedException");
       if (error.name === "NotAuthorizedException") {
         createAlert(
           "認証済み",
@@ -45,12 +43,7 @@ export default function Comfirmation({ route, navigation }) {
     }
   }
   return (
-    <View
-      style={[
-        globalStyles.flexColumn,
-        { width: "100%", height: "100%", marginVertical: 20 },
-      ]}
-    >
+    <View style={[globalStyles.flexColumn, styles.container]}>
       <View style={styles.inputContainer}>
         <Text style={globalStyles.inputLabel}>
           登録済みのメールアドレスに認証コードを送付しました。
@@ -76,33 +69,15 @@ export default function Comfirmation({ route, navigation }) {
       )}
       <TouchableOpacity onPress={() => confirmSignUp()}>
         <Text
-          style={[
-            globalStyles.textBtn,
-            {
-              backgroundColor: Colors.primary1,
-              width: 200,
-              marginVertical: 5,
-              textAlign: "center",
-            },
-          ]}
+          style={[globalStyles.textBtn, styles.btn, { backgroundColor: Colors.primary1 }]}
         >
           送信
         </Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => resendConfirmationCode()}>
         <Text
-          style={[
-            globalStyles.textBtn,
-            {
-              backgroundColor: Colors.primary2,
-              width: 200,
-              marginVertical: 5,
-              textAlign: "center",
-            },
-          ]}
-        >
-          認証コードを再送する
-        </Text>
+          style={[globalStyles.textBtn, styles.btn, { backgroundColor: Colors.primary2 }]}
+        >{`認証コードを\n再送する`}</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => navigation.navigate("SignIn")}>
         <Text style={globalStyles.textLink}>サインインページへ</Text>
@@ -111,6 +86,11 @@ export default function Comfirmation({ route, navigation }) {
   );
 }
 const styles = StyleSheet.create({
+  btn: {
+    width: 200,
+    marginVertical: 5,
+    textAlign: "center",
+  },
   extraLargeLogo: {
     width: 200,
     height: 200,
@@ -118,4 +98,5 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: 280,
   },
+  container: { width: "100%", height: "100%", marginVertical: 20 },
 });
